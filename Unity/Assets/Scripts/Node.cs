8ff74585fed69f7a18d57e28a5887d32647a49b2
Martin Pluisch <martin.pluisch@icloud.com>
2019-12-04 01:02:33 +0100

    - updated slides - updated folder structure

--- Unity/Assets/Scripts/Node.cs
+++ Unity/Assets/Scripts/Node.cs
@@ -14,24 +14,24 @@ public class Node {
     public int movedCarID;
     public Direction movedDirection;
 
-    public Node (Node parent, ParkingLot parkingLot, int movedCarID, Direction movedDirection) {
+    public Node(Node parent, ParkingLot parkingLot, int movedCarID, Direction movedDirection) {
         this.parent = parent;
-        this.parkingLot = parkingLot.Copy ();
+        this.parkingLot = parkingLot.Copy();
         this.movedCarID = movedCarID;
         this.movedDirection = movedDirection;
     }
 
-    public string GetParkingLotString () {
+    public string GetParkingLotString() {
         string result = "";
-        for (int i = 0; i < this.parkingLot.area.GetLength (0); i++) {
-            for (int j = 0; j < this.parkingLot.area.GetLength (1); j++) {
+        for (int i = 0; i < this.parkingLot.area.GetLength(0); i++) {
+            for (int j = 0; j < this.parkingLot.area.GetLength(1); j++) {
                 result += this.parkingLot.area[j, i];
             }
         }
         return result;
     }
 
-    public bool IsParkingLotSolved () {
-        return (parkingLot.cars[0].position.x + parkingLot.cars[0].length == parkingLot.area.GetLength (0));
+    public bool IsParkingLotSolved() {
+        return (parkingLot.cars[0].position.x + parkingLot.cars[0].length == parkingLot.area.GetLength(0));
     }
 }
